200aaa7a01f0966725139181462380f7aa5e55c3
d1mznpc <dimasakuq@gmail.com>
2025-06-19 12:48:40 +0700

    m

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Listings;
-use App\Livewire\Category;
-use App\Livewire\Location;
+use App\Livewire\Categorys;
+use App\Livewire\Locations;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('/produk', Listings::class)->name('listings.index');
+Route::get('/produk', Listings::class)->name('listings');
+Route::get('/kategori', Categorys::class)->name('category');
+Route::get('/lokasi', Locations::class)->name('location');

22ca9aae81604d0ae25a6e35f4bea963ea34f99b
Rilzy <144298570+RIlz27@users.noreply.github.com>
2025-06-19 14:08:41 +0700

    m

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Listings;
 use App\Livewire\Categories;
+use App\Livewire\Index;
 use App\Livewire\Locations;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-<<<<<<< HEAD
 Route::get('/produk', Listings::class);
-Route::get('/kategori', Categorys::class);
+Route::get('/kategori', Categories::class);
 Route::get('/lokasi', Locations::class);
-Route::get('/', function () {
-    return redirect('/produk');
-});
-=======
-Route::get('/produk', Listings::class)->name('listings');
-Route::get('/kategori', Categories::class)->name('category');
-Route::get('/lokasi', Locations::class)->name('location');
->>>>>>> 3c8e17838aa1ab5b2c7e8a3f68b3358e80e1312b
+Route::get('/', Index::class);

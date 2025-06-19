fcacf013b23935110e468dd870b79387a01d348e
Rilzy <144298570+RIlz27@users.noreply.github.com>
2025-06-19 15:29:42 +0700

    mgit

--- resources/views/livewire/index.blade.php
+++ resources/views/livewire/index.blade.php
@@ -1,17 +1,22 @@
-<div>
-    <div class="relative bg-cover bg-center h-[500px]"
-        style="background-image: url('{{ asset('storage/bg-house.jpg') }}');">
-        <div class="relative z-10 max-w-4xl mx-auto px-4 py-32 text-white text-center">
-            <h1 class="text-4xl font-bold leading-snug">Lorem ipsum dolor sit amet,<br> consectetur adipiscing elit, sed
-                do eiusmod</h1>
+<div class="w-full h-screen">
+    <div class="relative w-full h-[500px] overflow-hidden">
+        <!-- Background image -->
+        <div class="absolute inset-0 bg-center bg-fixed brightness-50"
+            style="background-image: url('{{ asset('storage/bg-hous/*  */e.jpg') }}'); background-position: center; background-size: 1600px;">
+        </div>
+
+        <!-- Konten di atas gambar -->
+        <div class="relative z-10 max-w-6xl px-4 py-32 mx-auto text-white text-left">
+            <h1 class="text-4xl font-bold leading-snug">
+                Lorem ipsum dolor sit amet,<br>
+                consectetur adipiscing elit, sed do eiusmod
+            </h1>
 
             <div class="mt-8 bg-white p-4 rounded-xl inline-block">
                 <p class="text-black text-sm font-semibold mb-4 text-left">Pilih Kategori Yang Diinginkan</p>
                 <div class="flex gap-2">
-                    <button class="bg-orange-400 text-white px-4 py-2 rounded-full font-semibold">Properti
-                        Hunian</button>
-                    <button class="bg-gray-200 text-gray-700 px-4 py-2 rounded-full font-semibold">Properti
-                        Komersial</button>
+                    <button class="bg-orange-400 text-white px-4 py-2 rounded-full font-semibold">Properti Hunian</button>
+                    <button class="bg-gray-200 text-gray-700 px-4 py-2 rounded-full font-semibold">Properti Komersial</button>
                     <button class="bg-gray-200 text-gray-700 px-4 py-2 rounded-full font-semibold">Barang Mewah</button>
                 </div>
             </div>
